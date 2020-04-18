715f4219db8c62f8b9d9019a0a07158fbf9a5149
Catherine Price <catprice444@gmail.com>
2020-04-18 16:42:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.uppercase
+  elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
   else phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
